49f055028d230cae60e563cfdee9790e23312b3f
DhareppaS1 <DhareppaS1@verifone.com>
2021-02-10 09:23:52 +0530

    ABC-commit-04

---
+++ src/main/java/org/test/tempTest.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class tempTest {
+
+    public static void main(String[] args) {
+        callss(new List);
+        for(int i: res){
+            System.out.println(i);
+        }
+    }
+
+    private static ListNode callss(int[] arr) {
+    }
+
+
+}
