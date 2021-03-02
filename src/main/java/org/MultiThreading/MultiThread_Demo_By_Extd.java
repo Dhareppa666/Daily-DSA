ac1c9e4f72451301ffad74bc2a9ad0188652cefa
DhareppaS1 <DhareppaS1@verifone.com>
2021-03-02 15:41:53 +0530

    aaaa

--- src/main/java/org/MultiThreading/MultiThread_Demo_By_Extd.java
+++ src/main/java/org/MultiThreading/MultiThread_Demo_By_Extd.java
@@ -5,8 +5,9 @@ public class MultiThread_Demo_By_Extd {
     public static void main(String[] args) {
         int n = 8;
         for (int i = 1; i <= n; i++) {
-            //multiThread_Demo thread = new multiThread_Demo();
-            Thread thread = new Thread(new multiThread_Demo());
+            multiThread_Demo runnable = new multiThread_Demo();
+            Thread thread = new Thread(runnable);
+            //thread.setDaemon(true);
             thread.start();
         }
     }
