9f5036038c7ced89e8275632bc5544bc489dd09a
DhareppaS1 <DhareppaS1@verifone.com>
2021-05-09 11:08:32 +0530

    9th may changes

---
+++ src/main/java/org/arrays/Jumps_2.java
@@ -0,0 +1,23 @@
+package org.arrays;
+
+public class Jumps_2 {
+
+    public static void main(String[] args) {
+        System.out.println(jump(new int[]{2,3,1,1,4}));
+    }
+
+    public static int jump(int[] nums) {//2,3,1,1,4
+        int begin = 0, end=0, farthest = 0;
+        int jumps = 0;
+        for(int i=0; i<nums.length-1; i++){
+
+            farthest = Math.max(farthest, i+nums[i]);
+
+            if(i == end){
+                jumps++;
+                end = farthest;
+            }
+        }
+        return jumps;
+    }
+}
