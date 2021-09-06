21ab2d96d0a0b399ca93e0e30241737af58d26b8
DhareppaS1 <DhareppaS1@verifone.com>
2021-09-06 19:26:29 +0530

    ABCD

--- src/main/java/org/REAL_TIME_COMP_QUES/Recro/Sample01.java
+++ src/main/java/org/REAL_TIME_COMP_QUES/Recro/Sample01.java
@@ -2,6 +2,12 @@ package org.REAL_TIME_COMP_QUES.Recro;
 
 public class Sample01 {
     public static void main(String[] args) {
-
+        int val =10;
+        if ((val>10? val++: --val)<10){
+            System.out.println("MAC");
+        }
+        if (val<10){
+            System.out.println("PC");
+        }
     }
 }
