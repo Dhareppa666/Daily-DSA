7fe2ce8167ce623453f54be33a3e934444cdfbd4
d0s0jfn <d0s0jfn@homeoffice.wal-mart.com>
2022-10-01 12:01:56 +0530

    October 1

---
+++ src/main/java/org/patterns/Problems/NoOfDigits.java
@@ -0,0 +1,32 @@
+package org.patterns.Problems;
+
+public class NoOfDigits {
+    public static void main(String args[])
+    {
+        int n = 123456;
+        System.out.println("Number of digits in "+n+" is "+count_digits3(n));
+    }
+    static int count_digits(int n)
+    {
+        int digits = (int) Math.floor(Math.log10(n) + 1);
+        return digits;
+    }
+
+    static int count_digits2(int n)
+    {
+        String digits = String.valueOf(n);
+        return digits.length();
+    }
+
+    static int count_digits3(int n)
+    {
+        int counter = 0, x = n;
+        while(x>0){
+            x/=10;
+            counter++;
+        }
+        return counter;
+    }
+
+
+}
