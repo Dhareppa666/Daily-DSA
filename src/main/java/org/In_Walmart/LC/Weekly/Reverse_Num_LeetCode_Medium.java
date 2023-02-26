b19bf1f92c2817359f108b62ac00a84d766226bf
d0s0jfn <d0s0jfn@homeoffice.wal-mart.com>
2023-02-26 11:56:42 +0530

    Sunday ka Kamaal

---
+++ src/main/java/org/In_Walmart/LC/Weekly/Reverse_Num_LeetCode_Medium.java
@@ -0,0 +1,25 @@
+package org.In_Walmart.LC.Weekly;
+
+public class Reverse_Num_LeetCode_Medium {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int reverse(int x)
+    {
+        int result = 0;
+
+        while (x != 0)
+        {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result)
+            { return 0; }
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+    }
+}
