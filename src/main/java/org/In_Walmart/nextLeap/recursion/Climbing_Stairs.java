c02814ec2146f514a5d79bd63e76b4dafb3ed951
d0s0jfn <d0s0jfn@homeoffice.wal-mart.com>
2023-07-23 21:32:22 +0530

    Daily DSA

---
+++ src/main/java/org/In_Walmart/nextLeap/recursion/Climbing_Stairs.java
@@ -0,0 +1,26 @@
+package org.In_Walmart.nextLeap.recursion;
+
+public class Climbing_Stairs {
+
+    //TC: O(n)
+    //SC: O(n)
+    public static void main(String[] args) {
+        int n =3;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        int res = climbStairs(n, dp);
+        System.out.println(res);
+    }
+
+    public static int climbStairs(int n, int[] dp) {
+        if(n==1 || n==2) {
+            return n;
+        }
+        if(dp[n] !=0){
+            return dp[n];
+        }
+        dp[n] = climbStairs(n-1, dp) + climbStairs(n-2, dp);
+        return dp[n];
+    }
+}
