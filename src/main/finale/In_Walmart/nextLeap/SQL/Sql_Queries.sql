30c8e9e3bcfbc114a715c2f87f3c492d25a71e5a
d0s0jfn <d0s0jfn@homeoffice.wal-mart.com>
2023-09-04 18:40:14 +0530

    Daily DSA

--- src/main/finale/In_Walmart/nextLeap/SQL/Sql_Queries.sql
+++ src/main/finale/In_Walmart/nextLeap/SQL/Sql_Queries.sql
@@ -9,7 +9,16 @@ SELECT * FROM Customers WHERE City IN ('Paris','London');
 
 SELECT CustomerName, ContactName, Address FROM Customers WHERE Address IS NOT NULL;
 
+SELECT tweet_id from Tweets where LENGTH(content) > 15  // returns the length of the string measured in bytes.
+SELECT tweet_id from Tweets where CHAR_LENGTH(content) > 15  // returns the length of the string measured in characters.
+
 Easy:
 
-SELECT DISTINCT author_id as id from Views Where author_id = viewer_id ORDER BY id;
+SELECT DISTINCT author_id as id FROM Views Where author_id = viewer_id ORDER BY id;
+
+SELECT e2.unique_id AS unique_id, e1.name AS name
+FROM Employees as e1
+LEFT JOIN EmployeeUNI as e2
+ON e2.id=e1.id;  // USING(id)
+
 
