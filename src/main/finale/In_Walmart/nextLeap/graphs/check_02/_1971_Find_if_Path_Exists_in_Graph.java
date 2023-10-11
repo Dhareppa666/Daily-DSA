bb115e7ba92a86cbb4d602bd916a29905bafb9df
d0s0jfn <d0s0jfn@homeoffice.wal-mart.com>
2023-10-11 18:50:32 +0530

    Daily DSA

---
+++ src/main/finale/In_Walmart/nextLeap/graphs/check_02/_1971_Find_if_Path_Exists_in_Graph.java
@@ -0,0 +1,53 @@
+package main.finale.In_Walmart.nextLeap.graphs.check_02;
+
+import java.util.*;
+
+// Graph formation is O(V + E) and BFS is additional O(V + E).
+public class _1971_Find_if_Path_Exists_in_Graph {
+
+    List<LinkedList<Integer>> adjList;
+
+    private boolean seen;
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if (n <= 1) {
+            return true;
+        }
+        this.adjList = new ArrayList();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new LinkedList<>());
+        }
+        for (int i = 0; i < edges.length; i++) {
+            int src = edges[i][0];
+            int dest = edges[i][1];
+            addEdge(src, dest);
+            addEdge(dest, src);
+        }
+        if (adjList.get(source).contains(destination)) {
+            return true;
+        }
+        this.seen = false;
+        boolean[] visited = new boolean[n];
+        dfs(visited, source, destination);
+        return this.seen;
+    }
+
+    public void dfs(boolean[] visited, int source, int destination) {
+        if (!visited[source] && !this.seen) {
+            if (source == destination) {
+                this.seen = true;
+                return;
+            }
+            visited[source] = true;
+            for (Integer i : adjList.get(source)) {
+                dfs(visited, i, destination);
+            }
+        }
+    }
+
+    public void addEdge(int source, int dest) {
+        adjList.get(source).add(dest);
+        adjList.get(dest).add(source);
+    }
+
+}
