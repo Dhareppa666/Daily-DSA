84b5de27b25d55adf6216061583a7613c3d6b67e
d0s0jfn <d0s0jfn@homeoffice.wal-mart.com>
2024-03-09 10:45:53 +0530

    Multi Threading

--- src/main/finale/Java/multi_threading/Producer_Consumer.java
+++ src/main/finale/Java/multi_threading/Producer_Consumer.java
@@ -2,83 +2,78 @@ package main.finale.Java.multi_threading;
 
 import java.util.LinkedList;
 
-// Java program to implement solution of producer consumer problem.
 public class Producer_Consumer {
 
-    public static void main(String[] args) throws InterruptedException {
-        // Object of a class that has both produce() and consume() methods
+    public static void main(String[] args) {
         final PC pc = new PC();
 
-        // Create producer thread
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    pc.produce();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        Thread t1 = new Thread(() -> {
+            try {
+                pc.produce();
+            } catch (InterruptedException ex) {
 
-        // Create consumer thread
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    pc.consume();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
             }
-        });
+        }, "Thread-01");
 
-        // Start both threads
+        Thread t2 = new Thread(() -> {
+            try {
+                pc.consume();
+            } catch (InterruptedException ex) {
+
+            }
+        }, "Thread-02");
+
         t1.start();
         t2.start();
 
-        // t1 finishes before t2
-        t1.join();
-        t2.join();
     }
 
-    // This class has a list, producer (adds items to list and consumer (removes items).
-    public static class PC {
+    static class PC {
 
-        // Create a list shared by producer and consumer Size of list is 2.
-        volatile LinkedList<Integer> list = new LinkedList<>();
-        int capacity = 10;
+        public volatile LinkedList<Integer> list;
 
-        // Function called by producer thread
+        public int num;
+
+        public boolean isProduce;
+
+        public PC() {
+            this.list = new LinkedList<>();
+            this.isProduce = true;
+            this.num = 1;
+        }
+
         public void produce() throws InterruptedException {
-            int value = 0;
-            while (true) {
-                synchronized (this) {
-                    // producer thread waits while list is full
-                    while (list.size() == capacity)
+            synchronized (this) {
+                while (true) {
+                    if (!this.isProduce) {
                         wait();
-
-                    System.out.println("Producer produced-" + value);
-                    list.add(value++);// to insert the jobs in the list
-                    notify();// notifies the consumer thread that now it can start consuming
-
-                    Thread.sleep(1000);// makes the working of program easier to understand
+                    } else {
+                        System.out.println("Produced " + num);
+                        this.list.add(num);
+                        this.num++;
+                        this.isProduce = false;
+                        notify();
+                        Thread.sleep(1000);
+                    }
                 }
+
             }
         }
 
-        // Function called by consumer thread
         public void consume() throws InterruptedException {
-            while (true) {
-                synchronized (this) {
-                    while (list.size() == 0)// consumer thread waits while list is empty
+            synchronized (this) {
+                while (true) {
+                    if (this.isProduce) {
                         wait();
-
-                    int val = list.removeFirst();// to retrieve the first job in the list
-                    System.out.println("Consumer consumed-" + val);
-                    notify();// Wake up producer thread
-                    Thread.sleep(1000);
+                    } else {
+                        Integer ele = this.list.removeFirst();
+                        System.out.println("Consumed " + ele);
+                        this.isProduce = true;
+                        notify();
+                        Thread.sleep(1000);
+                    }
                 }
+
             }
         }
     }
